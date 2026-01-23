f31bc019ac7b5819fdac311192743eea250f63e4
ZalmirSantos <zalmirparedes@gmail.com>
2026-01-23 16:03:18 -0300

    Adicionar arquivos de projeto.

---
+++ AccountData.cs
@@ -0,0 +1,40 @@
+﻿using System.Globalization;
+
+namespace Account
+{
+    class AccountData
+    {
+        public int Id { get; private set; }
+        public string Name { get; set; }
+        public double Balance { get; private set; }
+
+        public AccountData(int id, string name) 
+        {
+            Id = id;
+            Name = name;
+            Balance = 0.0;
+        }
+        public AccountData(int id, string name, double balance)
+        {
+            Id = id;
+            Name = name;
+            Balance = balance;
+        }
+
+        public void Deposit(double value)
+        {
+            Balance += value;
+        }
+
+        public void Withdraw(double value)
+        {
+            Balance -= value + 5.00; 
+        }
+        public override string ToString()
+        {
+            return "Account " + Id + ", Holder: " + Name + ", Balance: $ " + Balance.ToString("F2", CultureInfo.InvariantCulture);
+        }
+         
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Globalization;
+
+namespace Account
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CultureInfo CI = CultureInfo.InvariantCulture;
+
+            Console.Write("Enter the account number: ");
+            int id = int.Parse(Console.ReadLine());
+            Console.Write("Enter the account holder's name: ");
+            string name = Console.ReadLine();
+            Console.Write("Will there be an initial deposit? (y/n)? ");
+            char choice = char.Parse(Console.ReadLine());
+            AccountData AC = new AccountData(id, name);
+
+            if (choice == 'y')
+            {
+                Console.Write("Enter the initial deposit amount: ");
+                double balance = double.Parse(Console.ReadLine(), CI);
+                AC = new AccountData(id, name, balance);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Account data:");
+            Console.WriteLine(AC);
+
+            Console.WriteLine();
+            Console.Write("Enter the deposit amount: ");
+            double value = double.Parse(Console.ReadLine(), CI);
+            AC.Deposit(value);
+            Console.WriteLine("Updated account details:");
+            Console.WriteLine(AC);
+
+            Console.WriteLine();
+            Console.Write("Enter an amount to withdraw: ");
+            value = double.Parse(Console.ReadLine(), CI);
+            AC.Withdraw(value);
+            Console.WriteLine("Updated account details:");
+            Console.WriteLine(AC);
+        }
+    }
+}
